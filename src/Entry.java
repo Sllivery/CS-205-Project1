159067edd1c51022b9933ad16a631079e2353fa1
Siwei Xing <luxness.int@gmail.com>
2023-05-14 13:35:34 -0700

    add A star function and fix UniformCostSearch bug

--- src/Entry.java
+++ src/Entry.java
@@ -1,17 +1,48 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Entry {
     public static void main(String[] args) {
+
+        int[][] state = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0, 8}
+        };
+
+        int[][] copy = new int[state.length][];
+        for (int i = 0; i < copy.length; i++) {
+            copy[i] = Arrays.copyOf(state[i], state[0].length);
+        }
+
         int[][] test = {
                 {1, 2, 3},
                 {4, 5, 6},
-                {7, 8, 9}
+                {7, 0, 8}
         };
 
         int[][] GOAL = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,0},
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0},
         };
-        UniformCostSearch ucs = new UniformCostSearch(GOAL);
-        ucs.printTool(test);
+        UniformCostSearch ucs = new UniformCostSearch(test, GOAL);
+        boolean solution = ucs.solution();
+        System.out.println(solution);
+
+
+//        int[] test = {1, 2, 3, 4};
+//        int[] test2 = {1, 2, 3, 4};
+//        HashSet<int[]> set = new HashSet<>();
+//        set.add(test);
+//        boolean contains = set.contains(test2);
+//        System.out.println(contains);
+//
+//        String tests1 = "1,2,3,4";
+//        String tests2 = "1,2,3,4";
+//        HashSet<String> set2 = new HashSet<>();
+//        set2.add(tests1);
+//        boolean contains1 = set2.contains(tests2);
+//        System.out.println(contains1);
     }
 }
--- src/UniformCostSearch.java
+++ src/UniformCostSearch.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class UniformCostSearch {
@@ -17,10 +18,16 @@ public class UniformCostSearch {
 
     public boolean solution() {
         PriorityQueue<Node> queue = new PriorityQueue<>();
+        HashSet<String> set = new HashSet<>();
         //initialize the queue
         queue.add(new Node(this.board, 0));
         while (!queue.isEmpty()) {
             Node currentNode = queue.poll();
+            if (set.contains(currentNode.toString())) {
+                continue;
+            } else {
+                set.add(currentNode.toString());
+            }
             int[][] state = currentNode.getState();
             if (Arrays.deepEquals(state, GOAL)) {
                 return true;
@@ -31,13 +38,18 @@ public class UniformCostSearch {
             for (int[] item : direction) {
                 int newRow = zeroPosition[0] + item[0];
                 int newCol = zeroPosition[1] + item[1];
-                if (newRow > 0 && newRow < state.length && newCol > 0 && newCol < state[0].length) { // if position is legal
+                if (newRow >= 0 && newRow < state.length && newCol >= 0 && newCol < state[0].length) { // if position is legal
+                    //make a copy
+                    int[][] copy = new int[state.length][];
+                    for (int i = 0; i < copy.length; i++) {
+                        copy[i] = Arrays.copyOf(state[i], state[0].length);
+                    }
                     // then sawp 0 position with these new Position
-                    int temp = state[zeroPosition[0]][zeroPosition[1]];
-                    state[zeroPosition[0]][zeroPosition[1]] = state[newRow][newCol];
-                    state[newRow][newCol] = temp;
+                    int temp = copy[zeroPosition[0]][zeroPosition[1]];
+                    copy[zeroPosition[0]][zeroPosition[1]] = copy[newRow][newCol];
+                    copy[newRow][newCol] = temp;
                     // push state that's after the swap into the queue
-                    Node newNode = new Node(state, currentNode.getCost() + 1);
+                    Node newNode = new Node(copy, currentNode.getCost() + 1);
                     queue.add(newNode);
                 }
             }
@@ -85,6 +97,17 @@ public class UniformCostSearch {
 
         public int getCost() {
             return cost;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            for (int[] ints : this.state) {
+                for (int j = 0; j < this.state[0].length; j++) {
+                    builder.append(ints[j]);
+                }
+            }
+            return builder.toString();
         }
 
         @Override
